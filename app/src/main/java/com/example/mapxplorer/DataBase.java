e84950f4d2ff8f242abf9725360ef28d93155d86
BitaJlik <vtkach4488@gmail.com>
2021-11-02 11:58:51 +0200

    Interactive with products in listview

--- app/src/main/java/com/example/mapxplorer/DataBase.java
+++ app/src/main/java/com/example/mapxplorer/DataBase.java
@@ -6,8 +6,10 @@ public class DataBase {
     public static ArrayList<Market> markets = new ArrayList<>();
     public static int id = 0;
     static {
-        markets.add(new Market("Атб",49.23152796510891, 28.406867686183425));
+        markets.add(new Market("Атб",49.23152796510891, 28.406867686183425)); // 0
         markets.add(new Market("А2б",49.23122796510891, 28.406867686183425));
+
+
         markets.get(0).getProducts().add(new Product("Молоко",20,2));
         markets.get(0).getProducts().add(new Product("Ковбаса",40,2));
         markets.get(0).getProducts().add(new Product("Сир",60,2));
@@ -15,6 +17,12 @@ public class DataBase {
         markets.get(0).getProducts().add(new Product("Хліб",16,2));
         markets.get(0).getProducts().add(new Product("Риба",50,2));
         markets.get(0).getProducts().add(new Product("Шоколад",30,2));
+        markets.get(0).getProducts().add(new Product("Вода Солока",15,2));
+        markets.get(0).getProducts().add(new Product("Вода Газована",30,2));
+        markets.get(0).getProducts().add(new Product("Вода Негазована",30,2));
+        markets.get(0).getProducts().add(new Product("Сосиски молочні",30,2));
+        markets.get(0).getProducts().add(new Product("Сосиски мисливські",30,2));
+        markets.get(0).getProducts().add(new Product("Живчик 2Л",30,2));
 
 //        markets.add(new Market("Магазин",49.23114320346306, 28.406530458796375));
 //        markets.add(new Market("Абт",49.23164320346306, 28.406530458796375));
--- app/src/main/java/com/example/mapxplorer/MarketShowProducts.java
+++ app/src/main/java/com/example/mapxplorer/MarketShowProducts.java
@@ -1,26 +1,56 @@
 package com.example.mapxplorer;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
-public class MarketShowProducts extends AppCompatActivity {
+public class MarketShowProducts extends AppCompatActivity  {
     private TextView text ;
+    private ListView listView;
+    private ArrayAdapter<CharSequence> adapter;
+   @SuppressLint("SetTextI18n")
    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_market_show_products);
 
         text = findViewById(R.id.texted);
-
+        listView = findViewById(R.id.list);
         Market market = DataBase.markets.get(DataBase.id);
         if(!market.getProducts().isEmpty())
             text.setText(market.getNameMarket());
+        else text.setText(market.getNameMarket() + "   -> Пусто");
+       ArrayList<String> arrayList = new ArrayList<>();
         for(Product product : market.getProducts()){
-            text.append("\n" + product.getNameProduct());
+            arrayList.add(product.getNameProduct() + "   "+product.getAmount()+"шт     " +product.getPrice() +" Грн");
         }
+        String[] strings = new String[arrayList.size()];
+        for(int i = 0;i< arrayList.size();i++){
+            strings[i] = arrayList.get(i);
+        }
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, strings);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                System.out.println("+++" + arrayList.get(position));
+                print(arrayList,position);
+            }
+        });
+        listView.setAdapter(adapter);
 
+    }
+    private void print(ArrayList<String> a, int pos){
+       Toast.makeText(this,a.get(pos),Toast.LENGTH_SHORT).show();
     }
 }
